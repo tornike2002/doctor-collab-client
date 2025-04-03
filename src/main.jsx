4a3c8e8d3b9f7bab93d1483d91f9ef9d1ad3e262
tornike2002 <toko.toka2000@gmail.com>
2025-04-03 22:34:22 +0400

    remove t

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-const queryClient = new QueryClient();t
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <StrictMode>
