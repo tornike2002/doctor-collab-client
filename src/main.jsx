70a10df812b0aa36bb7e4a030d60dcae341c6575
tornike2002 <toko.toka2000@gmail.com>
2025-04-03 22:32:30 +0400

    starter project

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const queryClient = new QueryClient();t
+createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+      <ToastContainer />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </StrictMode>
+  </QueryClientProvider>
+);
